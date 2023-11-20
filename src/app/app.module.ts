17c5feb35a004300008914beb904fb02838d92e7
JoseMarson <josevitor-oliveira@hotmail.com>
2023-11-20 15:19:18 -0300

    Commit Header and Quantity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QuantitySelectionComponent } from './components/quantity-selection/quantity-selection.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -20,13 +25,16 @@ import { QuantitySelectionComponent } from './components/quantity-selection/quan
     HeaderComponent,
     QuantitySelectionComponent,
 
-    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/quantity-selection/quantity-selection.component.ts
+++ src/app/components/quantity-selection/quantity-selection.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quantity-selection',
   templateUrl: './quantity-selection.component.html',
   styleUrls: ['./quantity-selection.component.scss']
 })
-export class QuantitySelectionComponent {
+export class QuantitySelectionComponent implements OnInit {
+  filosofosForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.filosofosForm = this.fb.group({
+      quantidadeFilosofos: [3, [Validators.min(3), Validators.max(7)]],
+    });
+  }
+  decrementarQuantidade() {
+    let quantidadeAtual = this.filosofosForm.value.quantidadeFilosofos;
+    if (quantidadeAtual > 3) {
+      quantidadeAtual--;
+      this.filosofosForm.patchValue({ quantidadeFilosofos: quantidadeAtual });
+    }
+  }
+
+  incrementarQuantidade() {
+    let quantidadeAtual = this.filosofosForm.value.quantidadeFilosofos;
+    if (quantidadeAtual < 7) {
+      quantidadeAtual++;
+      this.filosofosForm.patchValue({ quantidadeFilosofos: quantidadeAtual });
+    }
+  }
+  aplicarConfiguracoes() {
+    // Implementação da lógica para aplicar as configurações
+  }
 }
