ad4377bb2ffa070eda408e40b999fe73e98333a8
JoseMarson <josevitor-oliveira@hotmail.com>
2023-11-28 15:10:55 -0300

    Adding Navigation Menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  showNav: boolean = false;
 
+  toggleNav(): void {
+    this.showNav = !this.showNav;
+  }
 }
