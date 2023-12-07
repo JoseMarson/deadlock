8ab5540e0896a6f6b211073d94f3d4004329d65d
JoseMarson <josevitor-oliveira@hotmail.com>
2023-12-07 15:50:48 -0300

    Updating Philosophers Window

--- src/app/components/quantity-selection/logic-application-philosophers/logic-application-philosophers.component.ts
+++ src/app/components/quantity-selection/logic-application-philosophers/logic-application-philosophers.component.ts
@@ -1,20 +1,35 @@
-import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, ChangeDetectorRef  } from '@angular/core';
-import { QuantitySelectionComponent } from '../quantity-selection.component';
 
-@Component({
-  selector: 'app-logic-application-philosophers',
-  templateUrl: './logic-application-philosophers.component.html',
-  styleUrls: ['./logic-application-philosophers.component.scss']
-})
-export class LogicApplicationPhilosophersComponent implements OnChanges {
-  @Input() quantidadeFilosofos?: number=3;
-  //@Output() aplicarConfiguracoesEvent = new EventEmitter<number>();
-  constructor(private cdr: ChangeDetectorRef) {}
+import { Component, Input, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['quantidadeFilosofos']) {
-      console.log('Valor atualizado:', this.quantidadeFilosofos);
-      this.cdr.detectChanges(); 
+  @Component({
+    selector: 'app-logic-application-philosophers',
+    templateUrl: './logic-application-philosophers.component.html',
+    styleUrls: ['./logic-application-philosophers.component.scss']
+  })
+  export class LogicApplicationPhilosophersComponent implements OnChanges {
+    @Input() quantidadeFilosofos?: number = 3;
+    imagemFilosofo: string = '../../../assests/images/mesa3.png.png';
+    constructor (private cdr: ChangeDetectorRef) {}
+  
+    ngOnChanges(changes: SimpleChanges): void {
+      if (changes['quantidadeFilosofos']) {
+        console.log('Valor atualizado:', this.quantidadeFilosofos);
+        this.atualizarImagemFilosofo();
+        this.cdr.detectChanges();
+      }
     }
+
+  private atualizarImagemFilosofo(): void {
+    if (this.quantidadeFilosofos == 3) {
+      this.imagemFilosofo = '../../../assets/images/mesa3.png';
+    } else if (this.quantidadeFilosofos == 4) {
+      this.imagemFilosofo = '../../../assets/images/mesa4.png';
+    } else if (this.quantidadeFilosofos == 5) {
+      this.imagemFilosofo = '../../../assets/images/mesa5.png';
+    } else if (this.quantidadeFilosofos == 6) {
+      this.imagemFilosofo = '../../../assets/images/mesa6.png';
+    } else if (this.quantidadeFilosofos == 7) {
+      this.imagemFilosofo = '../../../assets/images/mesa7.png';
+    } 
   }
 }
