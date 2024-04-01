80053d733a2f96d0448fb1312cb6836341ed6cdf
JoseMarson <josevitor-oliveira@hotmail.com>
2024-04-01 09:48:07 -0300

    Philosophers State Table

--- src/app/components/quantity-selection/logic-application-philosophers/logic-application-philosophers.component.ts
+++ src/app/components/quantity-selection/logic-application-philosophers/logic-application-philosophers.component.ts
@@ -9,6 +9,7 @@ export class LogicApplicationPhilosophersComponent implements OnChanges {
   @Input() quantidadeFilosofos?: number = 3;
   imagemFilosofo: string = './assets/images/mesa3.png';
   filosofos: any[] = [];
+  private execucaoAtiva: boolean = false; 
 
   constructor(private cdr: ChangeDetectorRef) {}
 
@@ -17,6 +18,7 @@ export class LogicApplicationPhilosophersComponent implements OnChanges {
       console.log('Valor atualizado:', this.quantidadeFilosofos);
       this.atualizarImagemFilosofo();
       this.inicializarFilosofos();
+      this.iniciarTemporizador();
       this.cdr.detectChanges();
     }
   }
@@ -29,11 +31,57 @@ export class LogicApplicationPhilosophersComponent implements OnChanges {
 
   private inicializarFilosofos(): void {
     this.filosofos = [];
+    const randomIndex = Math.floor(Math.random() * (this.quantidadeFilosofos || 0));
     for (let i = 1; i <= (this.quantidadeFilosofos || 0); i++) {
+      const estadoInicial = i === randomIndex ? 'Comendo' : 'Pensando';
       this.filosofos.push({
         nome: `Filósofo ${i}`,
-        estado: 'Pensando'
+        estado: estadoInicial
       });
     }
+  }
+
+  private iniciarTemporizador(): void {
+    this.execucaoAtiva = true; 
+    setInterval(() => {
+      if (this.execucaoAtiva) {
+        this.resetarEAlterarEstados();
+        this.cdr.detectChanges();
+      }
+    }, 10000);
+  }
+
+  pausarExecucao(): void {
+    this.execucaoAtiva = false; 
+  }
+
+  retomarExecucao(): void {
+    this.execucaoAtiva = true; 
+  }
+
+  private resetarEAlterarEstados(): void {
+
+    const garfosDisponiveis = new Array(this.quantidadeFilosofos || 0).fill(true);
+
+
+    this.filosofos.forEach((filosofo, index) => {
+
+      const garfoEsquerdaDisponivel = garfosDisponiveis[index];
+      const garfoDireitaDisponivel = garfosDisponiveis[(index + 1) % (this.quantidadeFilosofos || 0)];
+
+      if (filosofo.estado === 'Comendo') {
+        garfosDisponiveis[index] = true;
+        garfosDisponiveis[(index + 1) % (this.quantidadeFilosofos || 0)] = true;
+        filosofo.estado = 'Pensando';
+      } else {
+        if (garfoEsquerdaDisponivel && garfoDireitaDisponivel) {
+          filosofo.estado = 'Comendo';
+          garfosDisponiveis[index] = false;
+          garfosDisponiveis[(index + 1) % (this.quantidadeFilosofos || 0)] = false;
+        } else {
+          filosofo.estado = 'Aguardando';
+        }
+      }
+    });
   }
 }
